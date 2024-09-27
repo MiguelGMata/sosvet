e9f7cedd3f0617c0da6e71bc9b765c3859d1e5ce
MiguelGMata <miguelgmata.dev@gmail.com>
2024-09-27 02:27:52 +0200

    fetch animals

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 8080 // Solo afecta al entorno de desarrollo
-  },
   build: {
-    outDir: 'dist' // La salida del build de Vite
+    outDir: 'dist',  // Asegúrate de que el directorio de salida sea 'dist'
+  },
+  server: {
+    port: 8080
   }
-});
+})
