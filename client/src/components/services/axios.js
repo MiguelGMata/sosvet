f76a66685a37b913662f89906f9fbedf22b3fe77
MiguelGMata <miguelgmata.dev@gmail.com>
2024-10-08 18:42:43 +0200

    Asegurando que mysql está en las dependencias

--- client/src/components/services/axios.js
+++ client/src/components/services/axios.js
@@ -1,33 +1,33 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'https://sosvet.vercel.app' : 'http://localhost:4000',
+  baseURL: process.env.NODE_ENV === 'production' ? 'https://sosvet.vercel.app/sos' : 'http://localhost:4000/sos',
   headers: {
     'Content-Type': 'application/json'
   }
 });
-  
+
 // Interceptor para añadir el token a las solicitudes si existe
 axiosInstance.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-        if(token) {
-            config.headers['Authorization'] = `Bearer ${token}`;
-        }
-        return config;
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+  return config;
 }, (error) => {
-    return Promise.reject(error);
+  return Promise.reject(error);
 });
 
 axiosInstance.interceptors.response.use(
-    (response) => {
-      console.log('Interceptor de respuesta:', response);
-      return response;
-    },
-    (error) => {
-      console.error('Error en el interceptor:', error);
-      return Promise.reject(error);
-    }
-  );
+  (response) => {
+    console.log('Interceptor de respuesta:', response);
+    return response;
+  },
+  (error) => {
+    console.error('Error en el interceptor:', error);
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
 
--- client/src/components/services/veterinarianServices.js
+++ client/src/components/services/veterinarianServices.js
@@ -1,14 +1,14 @@
 import axiosInstance from './axios';
 
 export const veterinarianGet = async () => {
-    try {
-      const response = await axiosInstance.get('/sos/veterinaires');
-      return response.data; 
-    } catch (error) {
-      if (error.response && error.response.data) {
-          throw new Error(error.response.data.message || 'Erreur a recharger les veterinaires');
-        } else {
-          throw new Error('Une erreur est survenue. Veuillez réessayer plus tard.');
-        }
+  try {
+    const response = await axiosInstance.get('/veterinaires');
+    return response.data;
+  } catch (error) {
+    if (error.response && error.response.data) {
+      throw new Error(error.response.data.message || 'Erreur a recharger les veterinaires');
+    } else {
+      throw new Error('Une erreur est survenue. Veuillez réessayer plus tard.');
     }
   }
+}
